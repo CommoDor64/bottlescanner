df11e409e80437d4bf71b0774a7c796b73cb4f40
Dor Cohen <commo64dor@gmail.com>
2019-10-20 13:35:29 +0200

    init

---
+++ App.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+// import Pepito from './lib/iota/pepito';
+class App extends Component {
+
+  render() {
+    return (
+      <Home navigation={{ navigate: this.props.navigation.navigate }} />
+    );
+  }
+}
+//
+class Home extends Component {
+  state = {
+    hasCameraPermission: null,
+    scanned1: false,
+    scanned2: false,
+    addr: "",
+    bottles: {},
+    stage: 0,
+    done: false,
+  };
+
+  async componentDidMount() {
+    this.getPermissionsAsync();
+  }
+
+  getPermissionsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  };
+
+  handleBarCodeScanned = ({ type, data }) => {
+    this.setState({ scanned1: true, addr: data, stage: this.state.stage + 1 });
+  };
+
+  handleBottleScanned = ({ type, data }) => {
+    this.setState({ scanned2: true, bottles: { ...this.state.bottles, [data]: { uid: data, amount: (this.state.bottles[data]) ? this.state.bottles[data].amount + 1 : 1 } }, stage: this.state.stage + 1 });
+  };
+
+  componentDidUpdate() {
+    console.log("state: ", this.state);
+  }
+
+  pay(addr, amount) {
+    this.setState({ done: true, stage: -1 }, () => { console.log('sending info') })
+  }
+  render() {
+    const { hasCameraPermission, scanned1, scanned2, stage, done } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    if (stage === 0) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+          }}>
+          <Text>Please Scan your Crypto Address</Text>
+          <BarCodeScanner
+            onBarCodeScanned={scanned1 ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        </View>)
+    }
+
+    if (stage === 1) {
+      return (<View>
+        <Text> {`Your Address is ${this.state.addr}, is that corrent?`}</Text>
+        <Button title='Yes, continue to scan bottles' onPress={() => this.setState({ stage: this.state.stage + 1 })}></Button>
+        <Button title='No, Go back, I want to re-scan' onPress={() => this.setState({ stage: this.state.stage - 1, scanned1: false })}></Button>
+      </View>)
+    }
+
+    if (stage === 2) {
+      return (<View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }}>
+        <Text>Please Scan your Crypto Address</Text>
+        <BarCodeScanner
+          onBarCodeScanned={scanned2 ? undefined : this.handleBottleScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>)
+    }
+    if (stage === 3) {
+      return (<View>
+        <Text> Thank you!, Would you like to scan another bottle?</Text>
+        <Button title='Yes' onPress={() => this.setState({ stage: this.state.stage - 1, scanned2: false })}></Button>
+        <Button title='No, no I am done' onPress={() => this.pay(this.state.addr, 120)}></Button>
+      </View>)
+    }
+    if (done === true && stage === -1) {
+      const bottlesNum = Object.values(this.state.bottles).reduce((accu, item) => accu + item.amount, 0);
+      return (<View>
+        <Text> {`Thank you! you got 0.0002 ETH for ${bottlesNum} recycled`}</Text>
+      </View>)
+    }
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home,
+    params: {
+      title: `Bottle scanner, press next to start`,
+      nextScreen: 'BarcodeScanner'
+    }
+  }
+});
+
+export default createAppContainer(AppNavigator);
+
+const styles = StyleSheet.create({
+});
---
+++ components/BarcodeScanner/BarcodeScanner.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class BarcodeScanner extends React.Component {
+	state = {
+		hasCameraPermission: null,
+		scanned: false,
+	};
+
+	async componentDidMount() {
+		this.getPermissionsAsync();
+	}
+
+	getPermissionsAsync = async () => {
+		const { status } = await Permissions.askAsync(Permissions.CAMERA);
+		this.setState({ hasCameraPermission: status === 'granted' });
+	};
+
+	render() {
+		const { hasCameraPermission, scanned } = this.state;
+
+		if (hasCameraPermission === null) {
+			return <Text>Requesting for camera permission</Text>;
+		}
+		if (hasCameraPermission === false) {
+			return <Text>No access to camera</Text>;
+		}
+		return (
+			<View
+				style={{
+					flex: 1,
+					flexDirection: 'column',
+					justifyContent: 'flex-end',
+				}}>
+				<Text>Please Scan your Crypto Address</Text>
+				<BarCodeScanner
+					onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+					style={StyleSheet.absoluteFillObject}
+				/>
+			</View>
+		);
+	}
+
+	handleBarCodeScanned = ({ type, data }) => {
+		this.setState({ scanned: true });
+		const onPress = this.props.navigation.getParam('onPress');
+		onPress(data);
+		// this.props.navigation.navigate(this.props.navigation.getParam('nextScreen', 'Home'), { title: `Your account address ${data} was saved!, press next to scan bottles` })
+	};
+}
